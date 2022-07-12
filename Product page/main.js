d7278495f4c2134dd8b479fbb1833908bb6b0c87
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-07-12 21:09:44 +0300

    Реализация zoom

---
+++ Product page/main.js
@@ -0,0 +1,15 @@
+function hoverOnFirst(){
+    document.querySelectorAll('.product__view-main')[0].style.backgroundImage = "url('Photo/1.png')"
+}
+
+function hoverOnSecond(){
+    document.querySelectorAll('.product__view-main')[0].style.backgroundImage = "url('Photo/2.png')"
+}
+
+function hoverOnThree(){
+    document.querySelectorAll('.product__view-main')[0].style.backgroundImage = "url('Photo/3.png')"
+}
+
+function hoverBack(){
+    document.querySelectorAll('.product__view-main')[0].style.backgroundImage = "url('Photo/Main.png')"
+}
