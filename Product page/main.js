cdf10d7e434d965237c3cdffc6002216c45db77d
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-07-14 00:02:28 +0300

    Реализация счетчика

--- Product page/main.js
+++ Product page/main.js
@@ -13,3 +13,32 @@ function hoverOnThree(){
 function hoverBack(){
     document.querySelectorAll('.product__view-main')[0].style.backgroundImage = "url('Photo/Main.png')"
 }
+
+$(document).ready(function() {
+    $("body").on('focusout', '.product__counter-value', function(){
+        if($(this).val() < 1){
+            $(this).val('1');
+        }
+        if($(this).val() > 100){
+            $(this).val('100');
+        }
+        if(!Number.isInteger($(this).val())){
+            $(this).val(Math.round($(this).val()));
+        }
+    });
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) + 1;
+        count = count > 100 ? 100 : count;
+        $input.val(count);
+    });
+    
+  
+});	
