f54acfadb785409dcc502a382ec7be4d824b312f
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2022-07-31 00:47:59 +0300

    Delete PHP directory

--- PHP/SQL/requests.sql
+++
@@ -1,7 +0,0 @@
-SELECT category.category_id, category.title, count(category_product.product_id) as count_product
-FROM category 
-JOIN category_product ON category.category_id = category_product.category_id
-JOIN product ON product.product_id = category_product.product_id
-WHERE product.in_stock = 1
-GROUP BY category.category_id, category.title
-ORDER BY count_product DESC
