59cf99064156e039602834fd39515be08adcd74b
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2022-07-31 00:44:04 +0300

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,4 @@
 <?php
-    define('READFILE', true);
-    $servername = "localhost";
-    $database = "bootcamp_sivirilova";
-    $username = "root";
-    $password = "12345";
     // Создаем соединение
     $db = new PDO('mysql:host=localhost;dbname=clothing_store','root','12345');
     // Проверяем соединение
